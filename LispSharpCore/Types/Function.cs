0a35081afe12aa4b625c6ae3b153596415f9bed7
UnizaMatBre <breznican1@stud.uniza.sk>
2024-06-03 23:34:02 +0200

    Added unfinished class representing function

---
+++ LispSharpCore/Types/Function.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LispSharpCore.Types {
+    
+    /**
+     * Class representing callable function
+     */
+    public class Function { 
+
+
+        /**
+         * Constructs functions from provided parameters
+         *
+         * \param parameters : parameters of function
+         * \param variables : local variables of function - symbols from "parameters" are allowed
+         * \param code : source code of function
+         */
+        public Function(List<Symbol> parameters, List<Symbol> variables, List<object?> code) {
+            // empty for now        
+        }
+    
+    }
+}
