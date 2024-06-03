011311af723a4b780f542a62a0ad9e25605606b3
UnizaMatBre <breznican1@stud.uniza.sk>
2024-06-04 01:56:46 +0200

    Added unfinished class representing execution frame

--- LispSharpCore/Interpreter.cs
+++ LispSharpCore/Interpreter.cs
@@ -1,10 +1,23 @@
-﻿using System;
+﻿using LispSharpCore.Types;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace LispSharpCore {
+
+
+    /**
+     * Class representing running piece of code
+     */
+    internal class Frame {
+        
+
+        Frame(Function function, Frame? previous) { }
+    }
+
+
 
     /**
      * Class representing running execution of specific method
