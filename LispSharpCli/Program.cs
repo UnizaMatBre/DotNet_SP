9068be02be5de8db27ff07686353b4bfcf010233
UnizaMatBre <breznican1@stud.uniza.sk>
2024-06-03 14:43:19 +0200

    Added project for standalone command line app and added dependency to Lisp# core

---
+++ LispSharpCli/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LispSharpCli
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
