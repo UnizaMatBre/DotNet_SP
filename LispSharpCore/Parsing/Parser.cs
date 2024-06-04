4243a885d8ca7952e977874084a89e4c842dee8c
UnizaMatBre <breznican1@stud.uniza.sk>
2024-06-04 17:46:17 +0200

    Added fields into Parser

--- LispSharpCore/Parsing/Parser.cs
+++ LispSharpCore/Parsing/Parser.cs
@@ -6,10 +6,22 @@ using System.Threading.Tasks;
 
 namespace LispSharpCore.Parsing {
     public class Parser {
+        static readonly List<string> EmptyList = new List<string>();
 
+        // list of tokens that are consumed by parser
+        private List<string> _tokens = EmptyList;
+
+        // parser's position in token list
+        private int _index;
 
 
         public List<object?> Parse(string sourceCode) {
+            // extremly basic tokenization
+            this._tokens = sourceCode.Replace("(", " ( ").Replace(")", " ) ").Split(" ").ToList();
+            this._index = 0;
+
+
+
             return new List<object?>();
         }
     
