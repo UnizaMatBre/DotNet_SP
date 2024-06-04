1b77d9ae35e1dcf607f3cb90c3007f0d76e02cb3
UnizaMatBre <breznican1@stud.uniza.sk>
2024-06-04 17:27:09 +0200

    Added unfinished method for parsing

--- LispSharpCore/Parsing/Parser.cs
+++ LispSharpCore/Parsing/Parser.cs
@@ -5,5 +5,13 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace LispSharpCore.Parsing {
-    public class Parser { }
+    public class Parser {
+
+
+
+        public List<object?> Parse(string sourceCode) {
+            return new List<object?>();
+        }
+    
+    }
 }
