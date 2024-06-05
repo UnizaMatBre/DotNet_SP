f54eda1e47352d1e4d08020701baa5b0c209373a
UnizaMatBre <breznican1@stud.uniza.sk>
2024-06-05 22:21:10 +0200

    Implemented handling for special terms (except functions)

--- LispSharpCore/Interpreter.cs
+++ LispSharpCore/Interpreter.cs
@@ -1,6 +1,7 @@
 ﻿using LispSharpCore.Types;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -45,6 +46,80 @@ namespace LispSharpCore {
          * \return result of successful function call
          */
         private object? EvaluateCall(List<object?> list, Context context) {
+            // return empty list as is it
+            if (!list.Any()) {
+                return list;
+            }
+
+            var head = list.First();
+            var args = list.Skip(1).ToList();
+
+            if (head is Types.Symbol name) {
+
+                switch (name.Text) {
+                    
+                    // returns argument as it is, without evaluating it
+                    case "quote":
+                        if (args.Count() != 1) {
+                            throw new Exception("Special form #quote: wrong argument count");
+                        }
+                        return args.First();
+
+                       
+
+                    // evaluates condition and then evaluates true or false branch
+                    case "if":
+                        if (args.Count() != 3) {
+                            throw new Exception("Special form #if: wrong argument count");
+                        }
+
+                        var condition = this.EvaluateExpression(args[0], context);
+
+                        if (condition is bool result) {
+                            var correctBranch = (result) ? args[1] : args[2];
+
+                            return this.EvaluateExpression(correctBranch, context);
+                        }
+                        else { 
+                           throw new Exception("Special form #if: condition is not boolean");
+                        }
+
+                        
+                    // stores value into specified variable
+                    case "set":
+                        if (args.Count() != 2) {
+                            throw new Exception("Special form #set: wrong argument count");
+                        }
+
+                        if (args[0] is not Types.Symbol varName) {
+                            throw new Exception("Special form #set: variable name must be a symbol");
+                        }
+
+                        var storedVal = this.EvaluateExpression(args[1], context);
+
+                        if (!context.TryPutValue(varName, storedVal) {
+                            throw new Exception(String.Format("Special form #set: variable nammed #{0} was not found", varName.Text));
+                        }
+
+                        return storedVal;
+
+
+                    // creates new function and returns it
+                    case "function":
+                        if (args.Count() != 3) {
+                            throw new Exception("Special form #if wrong argument count");
+                        }
+                        throw new NotImplementedException();
+                        
+
+                    default:
+                        break;
+
+                }
+
+
+            }
+
             return null;
         }
 
