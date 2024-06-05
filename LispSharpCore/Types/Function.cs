618bf7bb9daa1780693cd1d39b24f870dadc55ca
UnizaMatBre <breznican1@stud.uniza.sk>
2024-06-05 22:46:08 +0200

    Added fields and constructor to the function

--- LispSharpCore/Types/Function.cs
+++ LispSharpCore/Types/Function.cs
@@ -101,6 +101,40 @@ namespace LispSharpCore.Types {
      * Class representing callable function
      */
     public class Function {
-      
+
+        // parameter names
+        public IList<Types.Symbol> Parameters { get; init; }
+
+        // variable names - automaticaly includes parameter names
+        public IList<Types.Symbol> Variables { get; init; }
+
+        // code of function
+        public IList<Object?> Code { get; init; }
+
+        // outer context where function was created 
+        public Context? OuterContext { get; init; }
+
+        // arity - number of parameters function has
+        public int Arity { get => this.Parameters.Count; }
+
+        /**
+         * Constructs functions from provided parameters
+         *
+         * \param parameters : parameters of function
+         * \param variables : local variables of function - symbols from "parameters" are allowed
+         * \param code : source code of function
+         * \param outerContext : context of outer function - used to create closure
+         */
+        public Function(IList<Types.Symbol> parameterNames, IList<Types.Symbol> variableNames, IList<Object?> code, Context context) {
+            if (parameterNames.Count() != parameterNames.Distinct().Count()) {
+                throw new Exception("Duplications in parameter list");
+            }
+            
+            this.Parameters     = parameterNames;
+            this.Variables      = variableNames.Union(parameterNames).ToList();
+            this.Code           = code;
+            this.OuterContext   = context;
+        }
+
     }
 }
