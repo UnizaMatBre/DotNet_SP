47166c3e03718f87e7ce2cd3a43f16d1229df8a6
UnizaMatBre <breznican1@stud.uniza.sk>
2024-06-05 18:12:25 +0200

    Added field for root context

--- LispSharpCore/Interpreter.cs
+++ LispSharpCore/Interpreter.cs
@@ -1,12 +1,21 @@
-﻿using System;
+﻿using LispSharpCore.Types;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace LispSharpCore {
+
     public class Interpreter {
-        
+
+        private Context _rootContext;
+
+        public Interpreter(Context rootContext) {
+            this._rootContext = rootContext;    
+        }
+
+
 
     }
 }
