a9e3797e6958a9818cc1952caa75026ebf118bb0
UnizaMatBre <breznican1@stud.uniza.sk>
2024-06-06 20:05:37 +0200

    Added class representing primitive

---
+++ LispSharpCore/Types/Primitive.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LispSharpCore.Types {
+
+    /**
+     * Class representing primitive function.
+     * 
+     * Main purpose is to extend abilities of lisp world
+     */
+    public class Primitive {
+        
+        public Func<Interpreter, Types.Context, IList<Object?>> Subroutine { get; init; }
+
+        public Primitive(Func<Interpreter, Types.Context, IList<Object?>> subroutine) { 
+            this.Subroutine = subroutine;
+        }
+
+        public override string ToString() {
+            return "Primitive " + this.Subroutine.ToString();
+        }
+
+    }
+}
