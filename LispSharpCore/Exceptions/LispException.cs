87ce29a5c51afb62479510318f0e593d92757484
UnizaMatBre <breznican1@stud.uniza.sk>
2024-06-06 10:19:30 +0200

    Added base lisp exception class

---
+++ LispSharpCore/Exceptions/LispException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LispSharpCore.Exceptions {
+    public abstract class LispException : Exception {
+
+        public LispException(string message) : base(message) { }
+    
+        public LispException(string message, Exception inner) : base(message, inner) { }
+
+    }
+}
