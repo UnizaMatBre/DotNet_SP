5e3959bab2b88820223e7f58e5338dea7c2729d8
UnizaMatBre <breznican1@stud.uniza.sk>
2024-06-06 19:40:13 +0200

    Added class representing world of objects

---
+++ LispSharpCore/LispWorld.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LispSharpCore {
+    /**
+     * Class representing world of lisp object
+     */
+    public class LispWorld {
+
+    }
+}
