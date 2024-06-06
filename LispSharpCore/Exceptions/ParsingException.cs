4034ac7b696e82f3c7aaada4b37225c29648b0d3
UnizaMatBre <breznican1@stud.uniza.sk>
2024-06-06 12:57:52 +0200

    Added exception for syntax errors

---
+++ LispSharpCore/Exceptions/ParsingException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LispSharpCore.Exceptions {
+    public class ParsingException : LispException {
+
+        public ParsingException(string message) : base(message) { }
+
+        public ParsingException(string message, Exception inner) : base(message, inner) { }
+    }
+}
